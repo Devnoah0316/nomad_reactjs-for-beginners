3be756298ffcceff68a780dfffbd9de0a4e587bc
Noah <noa316@naver.com>
2020-08-05 00:47:31 +0900

    using the movie db's(tmdb) api

---
+++ src/api.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+  params: {
+    api_key: "my api key",
+    language: "en-US",
+  },
+});
+
+export const moviesApi = {
+  nowPlaying: () => api.get("movie/now_playting"),
+  upComing: () => api.get("movie_upcoming"),
+  topRated: () => api.get("movie/top_rated"),
+  popular: () => api.get("movie/popular"),
+};
+
+export const tvApi = {
+  topRated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/popular"),
+  airingToday: () => api.get("tv/airing_today"),
+};
