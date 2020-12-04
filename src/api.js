62e2cc6dbf567c0203f6ae7e57b780f8c7551c07
noah0316 <noa316@naver.com>
2020-12-04 23:54:55 +0900

    test: Apply kr language

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: "https://api.themoviedb.org/3/",
   params: {
     api_key: process.env.REACT_APP_API_KEY,
-    language: "en-us",
+    language: "kr",
   },
 });
 
